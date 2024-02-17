fb09b6c1ebf77305089c854ce27799d48f821c49
KetchuppOfficial <dolgov04@list.ru>
2024-02-17 23:27:21 +0300

    A test for new methods was implemented

--- test/src/basic_algorithms.cpp
+++ test/src/basic_algorithms.cpp
@@ -24,3 +24,33 @@ TEST(Basics, Initializer_List_Constructor)
 
     EXPECT_EQ(g.n_edges(), 2);
 }
+
+TEST(Basics, Vertex_Degree)
+{
+    graphs::Directed_Graph g{1, 2, 3, 4};
+
+    auto it_1 = g.find_vertex(1);
+    auto it_2 = g.find_vertex(2);
+    auto it_3 = g.find_vertex(3);
+    auto it_4 = g.find_vertex(4);
+    g.insert_edge(it_2, it_1);
+    g.insert_edge(it_2, it_3);
+    g.insert_edge(it_2, it_4);
+    g.insert_edge(it_1, it_3);
+
+    EXPECT_EQ(g.vertex_in_degree(it_1), 1);
+    EXPECT_EQ(g.vertex_out_degree(it_1), 1);
+    EXPECT_EQ(g.vertex_degree(it_1), 2);
+
+    EXPECT_EQ(g.vertex_in_degree(it_2), 0);
+    EXPECT_EQ(g.vertex_out_degree(it_2), 3);
+    EXPECT_EQ(g.vertex_degree(it_2), 3);
+
+    EXPECT_EQ(g.vertex_in_degree(it_3), 2);
+    EXPECT_EQ(g.vertex_out_degree(it_3), 0);
+    EXPECT_EQ(g.vertex_degree(it_3), 2);
+
+    EXPECT_EQ(g.vertex_in_degree(it_4), 1);
+    EXPECT_EQ(g.vertex_out_degree(it_4), 0);
+    EXPECT_EQ(g.vertex_degree(it_4), 1);
+}
