eea0ded0ea9f8f3dd322fcfc20b22535e7ce4d42
KetchuppOfficial <dolgov04@list.ru>
2024-02-22 00:19:27 +0300

    Some updates in tests

--- test/src/basic_algorithms.cpp
+++ test/src/basic_algorithms.cpp
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
 
+#include <initializer_list>
+#include <algorithm>
+
 #include "graph.hpp"
 
-TEST(Basics, Default_Constructor)
+TEST(Graph, Default_Constructor)
 {
     graphs::Directed_Graph<int> g;
 
@@ -11,26 +14,101 @@ TEST(Basics, Default_Constructor)
     EXPECT_TRUE(g.empty());
 }
 
-TEST(Basics, Initializer_List_Constructor)
+TEST(Graph, Initializer_List_Constructor)
 {
     graphs::Directed_Graph g{1, 2, 3, 4};
 
     EXPECT_EQ(g.n_vertices(), 4);
+    EXPECT_EQ(g.n_edges(), 0);
     EXPECT_FALSE(g.empty());
+
+    auto list = {1, 2, 3, 4};
+    EXPECT_TRUE(std::ranges::equal(g, list)); // check that the order of vertices is preserved
 }
 
-TEST(Basics, Vertex_Degree)
+TEST(Graph, Insert_Vertex)
 {
     graphs::Directed_Graph<int> g;
+    auto list = {1, 2, 3, 4};
 
-    auto it_1 = g.insert_vertex(1);
-    auto it_2 = g.insert_vertex(2);
-    auto it_3 = g.insert_vertex(3);
-    auto it_4 = g.insert_vertex(4);
-    g.insert_edge(it_2, it_1);
-    g.insert_edge(it_2, it_3);
-    g.insert_edge(it_2, it_4);
-    g.insert_edge(it_1, it_3);
+    for (auto num : list)
+    {
+        auto it = g.insert_vertex(num);
+        EXPECT_EQ(*it, num);
+        EXPECT_EQ(g.n_vertices(), num);
+    }
+
+    EXPECT_EQ(g.n_edges(), 0);
+
+    EXPECT_TRUE(std::ranges::equal(g, list)); // check that the order of vertices is preserved
+}
+
+// TODO: insert_edge, insert_edges and are_adjacent should be tested here
+
+/*
+ *       +-- 1 --+
+ *       |   ^    |
+ *  2 <--+   |    +--> 3
+ *  |        |
+ *  +------> 4
+ */
+TEST(Graph, Erase_Vertex)
+{
+    graphs::Directed_Graph<int> g{1, 2, 3, 4};
+    auto it_1 = g.begin();
+    auto it_2 = std::next(it_1);
+    auto it_3 = std::next(it_2);
+    auto it_4 = std::next(it_3);
+
+    g.insert_edges({{it_1, it_2}, {it_1, it_3}, {it_2, it_4}, {it_4, it_1}});
+
+    EXPECT_EQ(g.n_vertices(), 4);
+    EXPECT_EQ(g.n_edges(), 4);
+    EXPECT_TRUE(g.are_adjacent(it_1, it_2));
+    EXPECT_TRUE(g.are_adjacent(it_1, it_3));
+    EXPECT_TRUE(g.are_adjacent(it_2, it_4));
+    EXPECT_TRUE(g.are_adjacent(it_4, it_1));
+    EXPECT_FALSE(g.are_adjacent(it_1, it_4));
+    EXPECT_FALSE(g.are_adjacent(it_2, it_1));
+    EXPECT_FALSE(g.are_adjacent(it_2, it_3));
+    EXPECT_FALSE(g.are_adjacent(it_3, it_1));
+    EXPECT_FALSE(g.are_adjacent(it_3, it_2));
+    EXPECT_FALSE(g.are_adjacent(it_3, it_4));
+    EXPECT_FALSE(g.are_adjacent(it_4, it_2));
+    EXPECT_FALSE(g.are_adjacent(it_4, it_3));
+
+    g.erase_vertex(it_2);
+
+    EXPECT_EQ(g.n_vertices(), 3);
+    EXPECT_EQ(g.n_edges(), 2);
+    EXPECT_TRUE(g.are_adjacent(it_1, it_3));
+    EXPECT_TRUE(g.are_adjacent(it_4, it_1));
+    EXPECT_FALSE(g.are_adjacent(it_1, it_4));
+    EXPECT_FALSE(g.are_adjacent(it_3, it_1));
+    EXPECT_FALSE(g.are_adjacent(it_3, it_4));
+    EXPECT_FALSE(g.are_adjacent(it_4, it_3));
+}
+
+/*
+ *   +------> 3
+ *   |        ^
+ *   |        |
+ *   |    +-- 2 --+
+ *   |    |       |
+ *   1 <--+       +--> 4
+ */
+TEST(Graph, Vertex_Degree)
+{
+    graphs::Directed_Graph<int> g{1, 2, 3, 4};
+    auto it_1 = g.begin();
+    auto it_2 = std::next(it_1);
+    auto it_3 = std::next(it_2);
+    auto it_4 = std::next(it_3);
+
+    g.insert_edges({{it_2, it_1},
+                    {it_2, it_3},
+                    {it_2, it_4},
+                    {it_1, it_3}});
 
     EXPECT_EQ(g.vertex_in_degree(it_1), 1);
     EXPECT_EQ(g.vertex_out_degree(it_1), 1);
