0d8d9d78eaee79549642768b01907486edd98870
KetchuppOfficial <dolgov04@list.ru>
2024-04-17 11:54:32 +0300

    More tests for Johnson's algorithm

--- test/unit-tests/src/johnson.cpp
+++ test/unit-tests/src/johnson.cpp
@@ -5,7 +5,7 @@
 #include "graph.hpp"
 #include "johnson.hpp"
 
-TEST(Johnson, Test)
+TEST(Johnson, Nonunique_Paths)
 {
     graphs::Directed_Graph<char> g;
     using iterator = decltype(g)::iterator;
@@ -23,6 +23,9 @@ TEST(Johnson, Test)
                     {it.at('c'), it.at('d'), 1}});
 
     graphs::Johnson apsp{g}; // apsp - all-pairs shortest paths
+
+    EXPECT_TRUE(apsp);
+    EXPECT_FALSE(apsp.has_negative_weight_cycles());
 
     EXPECT_EQ(apsp.distance(it.at('a'), it.at('a')), 0);
     EXPECT_EQ(apsp.distance(it.at('a'), it.at('b')), 2);
@@ -44,4 +47,76 @@ TEST(Johnson, Test)
     EXPECT_EQ(apsp.distance(it.at('d'), it.at('b')), inf);
     EXPECT_EQ(apsp.distance(it.at('d'), it.at('c')), inf);
     EXPECT_EQ(apsp.distance(it.at('d'), it.at('d')), 0);
+
+    g.change_weight(it.at('b'), it.at('a'), -3); // creating a negative weight cycle
+
+    graphs::Johnson apsp2{g};
+
+    EXPECT_FALSE(apsp2);
+    EXPECT_TRUE(apsp2.has_negative_weight_cycles());
+}
+
+// Example from "Introduction to Algorithms" by Thomas H. Cormen and others
+TEST(Johnson, From_Cormen)
+{
+    graphs::Directed_Graph<char> g;
+    using iterator = decltype(g)::iterator;
+
+    auto vertices = {'a', 'b', 'c', 'd', 'e'};
+
+    std::unordered_map<char, iterator> it;
+    for (auto v : vertices)
+        it.emplace(v, g.insert_vertex(v));
+
+    g.insert_edges({{it.at('a'), it.at('b'), 3},
+                    {it.at('a'), it.at('c'), 8},
+                    {it.at('a'), it.at('e'), -4},
+                    {it.at('b'), it.at('d'), 1},
+                    {it.at('b'), it.at('e'), 7},
+                    {it.at('c'), it.at('b'), 4},
+                    {it.at('d'), it.at('a'), 2},
+                    {it.at('d'), it.at('c'), -5},
+                    {it.at('e'), it.at('d'), 6}});
+
+    graphs::Johnson apsp{g}; // apsp - all-pairs shortest paths
+
+    EXPECT_TRUE(apsp);
+    EXPECT_FALSE(apsp.has_negative_weight_cycles());
+
+    EXPECT_EQ(apsp.distance(it.at('a'), it.at('a')), 0);
+    EXPECT_EQ(apsp.distance(it.at('a'), it.at('b')), 1);
+    EXPECT_EQ(apsp.distance(it.at('a'), it.at('c')), -3);
+    EXPECT_EQ(apsp.distance(it.at('a'), it.at('d')), 2);
+    EXPECT_EQ(apsp.distance(it.at('a'), it.at('e')), -4);
+
+    EXPECT_EQ(apsp.distance(it.at('b'), it.at('a')), 3);
+    EXPECT_EQ(apsp.distance(it.at('b'), it.at('b')), 0);
+    EXPECT_EQ(apsp.distance(it.at('b'), it.at('c')), -4);
+    EXPECT_EQ(apsp.distance(it.at('b'), it.at('d')), 1);
+    EXPECT_EQ(apsp.distance(it.at('b'), it.at('e')), -1);
+
+    EXPECT_EQ(apsp.distance(it.at('c'), it.at('a')), 7);
+    EXPECT_EQ(apsp.distance(it.at('c'), it.at('b')), 4);
+    EXPECT_EQ(apsp.distance(it.at('c'), it.at('c')), 0);
+    EXPECT_EQ(apsp.distance(it.at('c'), it.at('d')), 5);
+    EXPECT_EQ(apsp.distance(it.at('c'), it.at('e')), 3);
+
+    EXPECT_EQ(apsp.distance(it.at('d'), it.at('a')), 2);
+    EXPECT_EQ(apsp.distance(it.at('d'), it.at('b')), -1);
+    EXPECT_EQ(apsp.distance(it.at('d'), it.at('c')), -5);
+    EXPECT_EQ(apsp.distance(it.at('d'), it.at('d')), 0);
+    EXPECT_EQ(apsp.distance(it.at('d'), it.at('e')), -2);
+
+    EXPECT_EQ(apsp.distance(it.at('e'), it.at('a')), 8);
+    EXPECT_EQ(apsp.distance(it.at('e'), it.at('b')), 5);
+    EXPECT_EQ(apsp.distance(it.at('e'), it.at('c')), 1);
+    EXPECT_EQ(apsp.distance(it.at('e'), it.at('d')), 6);
+    EXPECT_EQ(apsp.distance(it.at('e'), it.at('e')), 0);
+
+    g.change_weight(it.at('a'), it.at('b'), -4); // creating a negative weight cycle
+
+    graphs::Johnson apsp2{g};
+
+    EXPECT_FALSE(apsp2);
+    EXPECT_TRUE(apsp2.has_negative_weight_cycles());
 }
