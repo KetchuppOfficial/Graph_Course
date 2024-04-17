7dd7396bd45620ea80de0a65883c90cec1eaf35e
KetchuppOfficial <dolgov04@list.ru>
2024-04-17 13:43:41 +0300

    Generator was significantly updated: edges' weights are generated, boost::program_options is used

--- test/end-to-end/src/generator.cpp
+++ test/end-to-end/src/generator.cpp
@@ -4,49 +4,122 @@
 #include <utility>
 #include <cstdlib>
 #include <stdexcept>
+#include <optional>
 
-std::pair<int, int> get_cmd_line_args(int argc, char *argv[])
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+class Options
 {
-    if (argc != 3)
-        throw std::runtime_error{"The program expects exactly 3 command line arguments"};
+public:
 
-    int n_vertices = std::atoi(argv[1]);
-    if (n_vertices <= 0)
-        throw std::runtime_error{"The number of vertices has to be a positive integer"};
+    Options(int argc, char *argv[])
+    {
+        po::options_description desc{"Allowed options"};
 
-    int n_edges = std::atoi(argv[2]);
-    if (n_vertices < 0)
-        throw std::runtime_error{"The number of vertices has to be a non-negative integer"};
+        desc.add_options()
+            ("help", "produce help message")
+            ("no-self-loops", "generate graph without self-loops")
+            ("n-vertices", po::value<int>(), "set the number of vertices")
+            ("n-edges", po::value<int>(), "set the number of edges")
+            ("max-weight-modulo", po::value<int>(), "set the maximal weight modulo");
 
-    if (n_edges > n_vertices * (n_vertices - 1))
-        throw std::runtime_error{"Too many edges"};
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
 
-    return std::pair{n_vertices, n_edges};
-}
+        if (vm.count("help"))
+        {
+            help_ = true;
+            std::cout << desc << std::endl;
+        }
+        else
+        {
+            help_ = false;
+
+            if (vm.count("n-vertices"))
+            {
+                v_ = vm["n-vertices"].as<int>();
+                if (v_ <= 0)
+                    throw std::runtime_error{"The number of vertices must be a positive number"};
+            }
+            else
+                throw std::runtime_error{"The number of vertices must be provided"};
+
+            if (vm.count("n-edges"))
+            {
+                e_ = vm["n-edges"].as<int>();
+                if (e_ <= 0)
+                    throw std::runtime_error{"The number of edges must be a positive number"};
+            }
+            else
+                throw std::runtime_error{"The number of edges must be provided"};
+
+            no_self_loops_ = vm.count("no-self-loops");
+
+            if (no_self_loops_)
+            {
+                if (e_ > v_ * (v_ - 1))
+                    throw std::runtime_error{"Too many edges"};
+            }
+            else if (e_ > v_ * v_)
+                throw std::runtime_error{"Too many edges"};
+
+            if (vm.count("max-weight-modulo"))
+            {
+                max_w_ = vm["max-weight-modulo"].as<int>();
+                if (max_w_ <= 0)
+                    throw std::runtime_error{"The maximal weight modulo must be a positive number"};
+            }
+            else
+                max_w_ = 20;
+        }
+    }
+
+    int n_vertices() const noexcept { return v_; }
+    int n_edges() const noexcept { return e_; }
+    int max_weight_modulo() const noexcept { return max_w_; }
+    bool no_self_loops() const noexcept { return no_self_loops_; }
+    bool help() const noexcept { return help_; }
+
+private:
+
+    int v_;
+    int e_;
+    int max_w_;
+    bool no_self_loops_;
+    bool help_;
+};
 
 int main(int argc, char *argv[])
 {
-    auto [n_vertices, n_edges] = get_cmd_line_args(argc, argv);
+    Options opts{argc, argv};
+    if (opts.help())
+        return 0;
 
-    for (auto v = 0; v != n_vertices; ++v)
+    for (auto v = 0; v != opts.n_vertices(); ++v)
         std::cout << "V " << v << ' ';
     std::cout << std::endl;
 
     std::random_device rd;
     std::mt19937_64 gen{rd()};
-    std::uniform_int_distribution<int> vertex{0, n_vertices - 1};
+    std::uniform_int_distribution<int> vertex{0, opts.n_vertices() - 1};
+    std::uniform_int_distribution<int> weight{-opts.max_weight_modulo(), opts.max_weight_modulo()};
     std::set<std::pair<int, int>> edges;
 
-    for (auto e = 0; e != n_edges;)
+    for (auto e = 0; e != opts.n_edges();)
     {
         int from = vertex(gen);
         int to = vertex(gen);
 
         if (auto edge = std::pair{from, to}; !edges.contains(edge))
         {
-            edges.emplace(edge);
-            std::cout << "E " << from << ' ' << to << ' ';
-            ++e;
+            if (!opts.no_self_loops() || from != to)
+            {
+                edges.emplace(edge);
+                std::cout << "E " << from << ' ' << to << ' ' << weight(gen) << ' ';
+                ++e;
+            }
         }
     }
 
