787c4b54b5d62f6e49561e58a0ec60161ad26cd4
KetchuppOfficial <dolgov04@list.ru>
2024-07-25 17:08:21 +0300

    add noexcept specifiers where necessary

--- include/distance.hpp
+++ include/distance.hpp
@@ -22,8 +22,8 @@ public:
 
     using value_type = T;
 
-    Distance() : is_inf_{true} {}
-    Distance(T w) : w_{w}, is_inf_{false} {} // we want implicit conversion
+    Distance() noexcept : is_inf_{true} {}
+    Distance(T w) noexcept : w_{w}, is_inf_{false} {} // we want implicit conversion
 
     bool is_inf() const noexcept { return is_inf_; }
     T operator*() const noexcept { return w_; } // using if is_inf() == true is UB
@@ -34,7 +34,7 @@ public:
         return w_;
     }
 
-    static Distance inf() { return Distance{}; }
+    static Distance inf() noexcept { return Distance{}; }
 
 private:
 
@@ -45,7 +45,7 @@ private:
 // Less than
 
 template<arithmetic T>
-bool operator<(Distance<T> lhs, Distance<T> rhs)
+bool operator<(Distance<T> lhs, Distance<T> rhs) noexcept
 {
     if (lhs.is_inf())
         return false;
@@ -56,48 +56,48 @@ bool operator<(Distance<T> lhs, Distance<T> rhs)
 }
 
 template<arithmetic T>
-bool operator<(Distance<T> lhs, T rhs) { return lhs.is_inf() ? false : *lhs < rhs; }
+bool operator<(Distance<T> lhs, T rhs) noexcept { return lhs.is_inf() ? false : *lhs < rhs; }
 
 template<arithmetic T>
-bool operator<(T lhs, Distance<T> rhs) { return rhs.is_inf() ? true : lhs < *rhs; }
+bool operator<(T lhs, Distance<T> rhs) noexcept { return rhs.is_inf() ? true : lhs < *rhs; }
 
 // Greater than or equal to
 
 template<arithmetic T>
-bool operator>=(Distance<T> lhs, Distance<T> rhs) { return !(lhs < rhs); }
+bool operator>=(Distance<T> lhs, Distance<T> rhs) noexcept { return !(lhs < rhs); }
 
 template<arithmetic T>
-bool operator>=(Distance<T> lhs, T rhs) { return !(lhs < rhs); }
+bool operator>=(Distance<T> lhs, T rhs) noexcept { return !(lhs < rhs); }
 
 template<arithmetic T>
-bool operator>=(T lhs, Distance<T> rhs) { return !(lhs < rhs); }
+bool operator>=(T lhs, Distance<T> rhs) noexcept { return !(lhs < rhs); }
 
 // Greater than
 
 template<arithmetic T>
-bool operator>(Distance<T> lhs, Distance<T> rhs) { return rhs < lhs; }
+bool operator>(Distance<T> lhs, Distance<T> rhs) noexcept { return rhs < lhs; }
 
 template<arithmetic T>
-bool operator>(Distance<T> lhs, T rhs) { return rhs < lhs; }
+bool operator>(Distance<T> lhs, T rhs) noexcept { return rhs < lhs; }
 
 template<arithmetic T>
-bool operator>(T lhs, Distance<T> rhs) { return rhs < lhs; }
+bool operator>(T lhs, Distance<T> rhs) noexcept { return rhs < lhs; }
 
 // Less than or equal to
 
 template<arithmetic T>
-bool operator<=(Distance<T> lhs, Distance<T> rhs) { return !(rhs < lhs); }
+bool operator<=(Distance<T> lhs, Distance<T> rhs) noexcept { return !(rhs < lhs); }
 
 template<arithmetic T>
-bool operator<=(Distance<T> lhs, T rhs) { return !(rhs < lhs); }
+bool operator<=(Distance<T> lhs, T rhs) noexcept { return !(rhs < lhs); }
 
 template<arithmetic T>
-bool operator<=(T lhs, Distance<T> rhs) { return !(rhs < lhs); }
+bool operator<=(T lhs, Distance<T> rhs) noexcept { return !(rhs < lhs); }
 
 // Equal
 
 template<arithmetic T>
-bool operator==(Distance<T> lhs, Distance<T> rhs)
+bool operator==(Distance<T> lhs, Distance<T> rhs) noexcept
 {
     if (lhs.is_inf())
         return rhs.is_inf();
@@ -106,15 +106,15 @@ bool operator==(Distance<T> lhs, Distance<T> rhs)
 }
 
 template<arithmetic T>
-bool operator==(Distance<T> lhs, T rhs) { return lhs.is_inf() ? false : *lhs == rhs; }
+bool operator==(Distance<T> lhs, T rhs) noexcept { return lhs.is_inf() ? false : *lhs == rhs; }
 
 template<arithmetic T>
-bool operator==(T lhs, Distance<T> rhs) { return rhs.is_inf() ? false : lhs == *rhs; }
+bool operator==(T lhs, Distance<T> rhs) noexcept { return rhs.is_inf() ? false : lhs == *rhs; }
 
 // Sum
 
 template<arithmetic T>
-Distance<T> operator+(Distance<T> &lhs, Distance<T> &rhs)
+Distance<T> operator+(Distance<T> &lhs, Distance<T> &rhs) noexcept
 {
     if (lhs.is_inf() || rhs.is_inf())
         return Distance<T>::inf();
@@ -123,13 +123,13 @@ Distance<T> operator+(Distance<T> &lhs, Distance<T> &rhs)
 }
 
 template<arithmetic T>
-Distance<T> operator+(Distance<T> lhs, T rhs)
+Distance<T> operator+(Distance<T> lhs, T rhs) noexcept
 {
     return lhs.is_inf() ? Distance<T>::inf() : *lhs + rhs;
 }
 
 template<arithmetic T>
-Distance<T> operator+(T lhs, Distance<T> rhs)
+Distance<T> operator+(T lhs, Distance<T> rhs) noexcept
 {
     return rhs + lhs;
 }
