0fc45a5ce21a637564563d5434f334144ed61e6a
KetchuppOfficial <dolgov04@list.ru>
2024-07-27 18:18:46 +0300

    add simple tests for Knuth's graph

---
+++ test/unit-tests/src/kgraph.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+
+#include <array>
+
+#include "tuple_like.hpp"
+#include "graphs/kgraph.hpp"
+
+TEST(KGraph, Initializer_List_Constructor)
+{
+    // construction from std::tuple with different vertex and edge types
+    graphs::KGraph g_1{std::tuple{1, 2, 'p'},
+                       std::tuple{1, 3, 'q'},
+                       std::tuple{2, 3, 'r'},
+                       std::tuple{2, 4, 's'},
+                       std::tuple{3, 4, 't'}};
+
+    EXPECT_EQ(g_1.n_vertices(), 4);
+    EXPECT_EQ(g_1.n_edges(), 5);
+
+    // construction from std::tuple with identical vertex and edge types
+    graphs::KGraph g_2{std::tuple{1, 2, 0},
+                       std::tuple{1, 3, 0},
+                       std::tuple{2, 3, 0},
+                       std::tuple{2, 4, 0},
+                       std::tuple{3, 4, 0}};
+
+    EXPECT_EQ(g_2.n_vertices(), 4);
+    EXPECT_EQ(g_2.n_edges(), 5);
+
+    // construction from a tuple-like standard class
+    static_assert(graphs::edge_initializer<std::array<int, 3>>);
+    graphs::KGraph g_3{std::array<int, 3>{1, 2, 0},
+                       std::array<int, 3>{1, 3, 0},
+                       std::array<int, 3>{2, 3, 0},
+                       std::array<int, 3>{2, 4, 0},
+                       std::array<int, 3>{3, 4, 0}};
+
+    EXPECT_EQ(g_3.n_vertices(), 4);
+    EXPECT_EQ(g_3.n_edges(), 5);
+
+    // construction from a tuple-like user-defined class
+    static_assert(graphs::trinity_like<TupleLike>);
+    graphs::KGraph g_4{TupleLike{1, 2, 'p'},
+                       TupleLike{1, 3, 'q'},
+                       TupleLike{2, 3, 'r'},
+                       TupleLike{2, 4, 's'},
+                       TupleLike{3, 4, 't'}};
+
+    EXPECT_EQ(g_4.n_vertices(), 4);
+    EXPECT_EQ(g_4.n_edges(), 5);
+}
---
+++ test/unit-tests/src/tuple_like.hpp
@@ -0,0 +1,61 @@
+#ifndef TEST_UNIT_TESTS_SRC_TUPLE_LIKE_HPP
+#define TEST_UNIT_TESTS_SRC_TUPLE_LIKE_HPP
+
+#include <tuple>
+
+// A structure that behaves similar to std::tuple<int, int, char>
+struct TupleLike { int v_1; int v_2; char e; };
+
+#define GET_IMPL                                                                                   \
+    static_assert(I < 3, "tuple index must be in range");                                          \
+                                                                                                   \
+    if constexpr (I == 0)                                                                          \
+        return t.v_1;                                                                              \
+    else if (I == 1)                                                                               \
+        return t.v_2;                                                                              \
+    else                                                                                           \
+        return t.e;
+
+#define MOVE_GET_IMPL                                                                              \
+    static_assert(I < 3, "tuple index must be in range");                                          \
+                                                                                                   \
+    if constexpr (I == 0)                                                                          \
+        return std::move(t.v_1);                                                                   \
+    else if (I == 1)                                                                               \
+        return std::move(t.v_2);                                                                   \
+    else                                                                                           \
+        return std::move(t.e);
+
+namespace std
+{
+
+template<>
+struct tuple_size<::TupleLike> : std::integral_constant<std::size_t, 3> {};
+
+template<>
+struct tuple_element<0, ::TupleLike> { using type = int; };
+
+template<>
+struct tuple_element<1, ::TupleLike> { using type = int; };
+
+template<>
+struct tuple_element<2, ::TupleLike> { using type = char; };
+
+template<std::size_t I>
+std::tuple_element<I, ::TupleLike>::type &get(::TupleLike &t) noexcept { GET_IMPL }
+
+template<std::size_t I>
+const std::tuple_element<I, ::TupleLike>::type &get(const ::TupleLike &t) noexcept { GET_IMPL }
+
+template<std::size_t I>
+std::tuple_element<I, ::TupleLike>::type &&get(::TupleLike &&t) noexcept { MOVE_GET_IMPL }
+
+template<std::size_t I>
+const std::tuple_element<I, ::TupleLike>::type &&get(const ::TupleLike &&t) noexcept { MOVE_GET_IMPL }
+
+} // namespace std
+
+#undef GET_IMPL
+#undef CONST_GET_IMPL
+
+#endif // TEST_UNIT_TESTS_SRC_TUPLE_LIKE_HPP
