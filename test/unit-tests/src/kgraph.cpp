1569462bd9e3ef2ffd73b896035d8ff7e55988e3
KetchuppOfficial <dolgov04@list.ru>
2024-07-28 11:48:31 +0300

    add static asserts on nested aliases of KGraph

--- test/unit-tests/src/kgraph.cpp
+++ test/unit-tests/src/kgraph.cpp
@@ -14,6 +14,8 @@ TEST(KGraph, Initializer_List_Constructor)
                        std::tuple{2, 4, 's'},
                        std::tuple{3, 4, 't'}};
 
+    static_assert(std::is_same_v<decltype(g_1)::vertex_type, int>);
+    static_assert(std::is_same_v<decltype(g_1)::edge_type, char>);
     EXPECT_EQ(g_1.n_vertices(), 4);
     EXPECT_EQ(g_1.n_edges(), 5);
 
@@ -24,6 +26,8 @@ TEST(KGraph, Initializer_List_Constructor)
                        std::tuple{2, 4, 0},
                        std::tuple{3, 4, 0}};
 
+    static_assert(std::is_same_v<decltype(g_2)::vertex_type, int>);
+    static_assert(std::is_same_v<decltype(g_2)::edge_type, int>);
     EXPECT_EQ(g_2.n_vertices(), 4);
     EXPECT_EQ(g_2.n_edges(), 5);
 
@@ -35,6 +39,8 @@ TEST(KGraph, Initializer_List_Constructor)
                        std::array<int, 3>{2, 4, 0},
                        std::array<int, 3>{3, 4, 0}};
 
+    static_assert(std::is_same_v<decltype(g_3)::vertex_type, int>);
+    static_assert(std::is_same_v<decltype(g_3)::edge_type, int>);
     EXPECT_EQ(g_3.n_vertices(), 4);
     EXPECT_EQ(g_3.n_edges(), 5);
 
@@ -46,6 +52,8 @@ TEST(KGraph, Initializer_List_Constructor)
                        TupleLike{2, 4, 's'},
                        TupleLike{3, 4, 't'}};
 
+    static_assert(std::is_same_v<decltype(g_4)::vertex_type, int>);
+    static_assert(std::is_same_v<decltype(g_4)::edge_type, char>);
     EXPECT_EQ(g_4.n_vertices(), 4);
     EXPECT_EQ(g_4.n_edges(), 5);
 }
