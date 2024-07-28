887f9306efcd6e285b7cc02c92d17de94870a5df
KetchuppOfficial <dolgov04@list.ru>
2024-07-28 11:38:14 +0300

    remove macros

--- test/unit-tests/src/tuple_like.hpp
+++ test/unit-tests/src/tuple_like.hpp
@@ -6,26 +6,6 @@
 // A structure that behaves similar to std::tuple<int, int, char>
 struct TupleLike { int v_1; int v_2; char e; };
 
-#define GET_IMPL                                                                                   \
-    static_assert(I < 3, "tuple index must be in range");                                          \
-                                                                                                   \
-    if constexpr (I == 0)                                                                          \
-        return t.v_1;                                                                              \
-    else if constexpr (I == 1)                                                                     \
-        return t.v_2;                                                                              \
-    else                                                                                           \
-        return t.e;
-
-#define MOVE_GET_IMPL                                                                              \
-    static_assert(I < 3, "tuple index must be in range");                                          \
-                                                                                                   \
-    if constexpr (I == 0)                                                                          \
-        return std::move(t.v_1);                                                                   \
-    else if constexpr (I == 1)                                                                     \
-        return std::move(t.v_2);                                                                   \
-    else                                                                                           \
-        return std::move(t.e);
-
 namespace std
 {
 
@@ -42,20 +22,45 @@ template<>
 struct tuple_element<2, ::TupleLike> { using type = char; };
 
 template<size_t I>
-tuple_element<I, ::TupleLike>::type &get(::TupleLike &t) noexcept { GET_IMPL }
+const tuple_element<I, ::TupleLike>::type &get(const ::TupleLike &t) noexcept
+{
+    static_assert(I < 3, "tuple index must be in range");
+
+    if constexpr (I == 0)
+        return t.v_1;
+    else if constexpr (I == 1)
+        return t.v_2;
+    else
+        return t.e;
+}
 
 template<size_t I>
-const tuple_element<I, ::TupleLike>::type &get(const ::TupleLike &t) noexcept { GET_IMPL }
+auto &get(::TupleLike &t) noexcept
+{
+    return const_cast<tuple_element<I,
+                      ::TupleLike>::type &>(get<I>(static_cast<const ::TupleLike &>(t)));
+}
 
 template<size_t I>
-tuple_element<I, ::TupleLike>::type &&get(::TupleLike &&t) noexcept { MOVE_GET_IMPL }
+const tuple_element<I, ::TupleLike>::type &&get(const ::TupleLike &&t) noexcept
+{
+    static_assert(I < 3, "tuple index must be in range");
+
+    if constexpr (I == 0)
+        return std::move(t.v_1);
+    else if constexpr (I == 1)
+        return std::move(t.v_2);
+    else
+        return std::move(t.e);
+}
 
 template<size_t I>
-const tuple_element<I, ::TupleLike>::type &&get(const ::TupleLike &&t) noexcept { MOVE_GET_IMPL }
+auto &&get(::TupleLike &&t) noexcept
+{
+    return const_cast<tuple_element<I,
+                      ::TupleLike>::type &&>(get<I>(static_cast<const ::TupleLike &&>(t)));
+}
 
 } // namespace std
-
-#undef GET_IMPL
-#undef CONST_GET_IMPL
 
 #endif // TEST_UNIT_TESTS_SRC_TUPLE_LIKE_HPP
