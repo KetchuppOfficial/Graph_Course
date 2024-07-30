feebb3a2050f12627ac06abfe89a8b73e478b42d
KetchuppOfficial <dolgov04@list.ru>
2024-07-30 14:14:24 +0300

    add a test for BFS on KGraph

--- test/unit-tests/src/bfs.cpp
+++ test/unit-tests/src/bfs.cpp
@@ -2,8 +2,9 @@
 
 #include "algorithms/bfs.hpp"
 #include "graphs/directed_graph.hpp"
+#include "graphs/kgraph.hpp"
 
-TEST(BFS, From_Cormen)
+TEST(BFS, Directed_Graph)
 {
     using G = graphs::Directed_Graph<char>;
     using size_type = graphs::graph_traits<G>::size_type;
@@ -42,4 +43,36 @@ TEST(BFS, From_Cormen)
 
     for (auto v : vertices)
         EXPECT_EQ(bfs.distance(it.at(v)), distance.at(v));
+}
+
+TEST(BFS, KGraph)
+{
+    auto vertices = {'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    graphs::KGraph g{std::tuple{'s', 'r', 0},
+                     std::tuple{'s', 'v', 0},
+                     std::tuple{'s', 'u', 0},
+                     std::tuple{'u', 't', 0},
+                     std::tuple{'u', 'y', 0},
+                     std::tuple{'r', 't', 0},
+                     std::tuple{'r', 'w', 0},
+                     std::tuple{'v', 'w', 0},
+                     std::tuple{'v', 'y', 0},
+                     std::tuple{'x', 'w', 0},
+                     std::tuple{'x', 'y', 0},
+                     std::tuple{'x', 'z', 0},
+                     std::tuple{'w', 'z', 0}};
+
+    graphs::BFS bfs{g, g.find_vertex('s').value()};
+
+    std::unordered_map<char, std::size_t> distance =
+    {
+        {'s', 0},
+        {'r', 1}, {'v', 1}, {'u', 1},
+        {'t', 2}, {'y', 2}, {'w', 2},
+        {'x', 3}, {'z', 3}
+    };
+
+    for (auto v : vertices)
+        EXPECT_EQ(bfs.distance(g.find_vertex(v).value()), distance.at(v));
 }
