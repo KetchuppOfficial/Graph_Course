da93b8d0a5a596f392eb853567c969f9c4fa9938
KetchuppOfficial <dolgov04@list.ru>
2024-08-05 21:59:46 +0300

    add tests for unweighted KGraph

--- test/unit-tests/src/kgraph.cpp
+++ test/unit-tests/src/kgraph.cpp
@@ -4,11 +4,14 @@
 #include <vector>
 #include <unordered_set>
 
+#include "pair_like.hpp"
 #include "tuple_like.hpp"
 #include "graphs/kgraph.hpp"
 
 static_assert(graphs::edge_initializer<TupleLike>);
 static_assert(graphs::edge_initializer<std::array<int, 3>>);
+static_assert(graphs::edge_initializer<PairLike>);
+static_assert(graphs::edge_initializer<std::array<int, 2>>);
 
 TEST(KGraph, Initializer_List_Constructor)
 {
@@ -21,6 +24,7 @@ TEST(KGraph, Initializer_List_Constructor)
 
     static_assert(std::is_same_v<decltype(g_1)::vertex_type, int>);
     static_assert(std::is_same_v<decltype(g_1)::edge_type, char>);
+    static_assert(decltype(g_1)::weighted());
     EXPECT_EQ(g_1.n_vertices(), 4);
     EXPECT_EQ(g_1.n_edges(), 5);
 
@@ -33,10 +37,11 @@ TEST(KGraph, Initializer_List_Constructor)
 
     static_assert(std::is_same_v<decltype(g_2)::vertex_type, int>);
     static_assert(std::is_same_v<decltype(g_2)::edge_type, int>);
+    static_assert(decltype(g_2)::weighted());
     EXPECT_EQ(g_2.n_vertices(), 4);
     EXPECT_EQ(g_2.n_edges(), 5);
 
-    // construction from a tuple-like standard class
+    // construction from a standard tuple-like class
     graphs::KGraph g_3{std::array<int, 3>{1, 2, 0},
                        std::array<int, 3>{1, 3, 0},
                        std::array<int, 3>{2, 3, 0},
@@ -45,10 +50,11 @@ TEST(KGraph, Initializer_List_Constructor)
 
     static_assert(std::is_same_v<decltype(g_3)::vertex_type, int>);
     static_assert(std::is_same_v<decltype(g_3)::edge_type, int>);
+    static_assert(decltype(g_3)::weighted());
     EXPECT_EQ(g_3.n_vertices(), 4);
     EXPECT_EQ(g_3.n_edges(), 5);
 
-    // construction from a tuple-like user-defined class
+    // construction from a user-defined tuple-like class
     graphs::KGraph g_4{TupleLike{1, 2, 'p'},
                        TupleLike{1, 3, 'q'},
                        TupleLike{2, 3, 'r'},
@@ -57,8 +63,57 @@ TEST(KGraph, Initializer_List_Constructor)
 
     static_assert(std::is_same_v<decltype(g_4)::vertex_type, int>);
     static_assert(std::is_same_v<decltype(g_4)::edge_type, char>);
+    static_assert(decltype(g_4)::weighted());
     EXPECT_EQ(g_4.n_vertices(), 4);
     EXPECT_EQ(g_4.n_edges(), 5);
+
+    // construction of unweighted graph from std::tuple
+    graphs::KGraph g_5{std::tuple{1, 2},
+                       std::tuple{1, 3},
+                       std::tuple{2, 3},
+                       std::tuple{2, 4},
+                       std::tuple{3, 4}};
+    static_assert(std::is_same_v<decltype(g_5)::vertex_type, int>);
+    static_assert(std::is_same_v<decltype(g_5)::edge_type, void>);
+    static_assert(!decltype(g_5)::weighted());
+    EXPECT_EQ(g_5.n_vertices(), 4);
+    EXPECT_EQ(g_5.n_edges(), 5);
+
+    // construction of unweighted graph from std::pair
+    graphs::KGraph g_6{std::pair{1, 2},
+                       std::pair{1, 3},
+                       std::pair{2, 3},
+                       std::pair{2, 4},
+                       std::pair{3, 4}};
+    static_assert(std::is_same_v<decltype(g_6)::vertex_type, int>);
+    static_assert(std::is_same_v<decltype(g_6)::edge_type, void>);
+    static_assert(!decltype(g_6)::weighted());
+    EXPECT_EQ(g_6.n_vertices(), 4);
+    EXPECT_EQ(g_6.n_edges(), 5);
+
+    // construction of unweighted graph from standard pair-like class
+    graphs::KGraph g_7{std::array<int, 2>{1, 2},
+                       std::array<int, 2>{1, 3},
+                       std::array<int, 2>{2, 3},
+                       std::array<int, 2>{2, 4},
+                       std::array<int, 2>{3, 4}};
+    static_assert(std::is_same_v<decltype(g_7)::vertex_type, int>);
+    static_assert(std::is_same_v<decltype(g_7)::edge_type, void>);
+    static_assert(!decltype(g_7)::weighted());
+    EXPECT_EQ(g_7.n_vertices(), 4);
+    EXPECT_EQ(g_7.n_edges(), 5);
+
+    // construction of unweighted graph from user-defined pair-like class
+    graphs::KGraph g_8{PairLike{1, 2},
+                       PairLike{1, 3},
+                       PairLike{2, 3},
+                       PairLike{2, 4},
+                       PairLike{3, 4}};
+    static_assert(std::is_same_v<decltype(g_8)::vertex_type, int>);
+    static_assert(std::is_same_v<decltype(g_8)::edge_type, void>);
+    static_assert(!decltype(g_8)::weighted());
+    EXPECT_EQ(g_8.n_vertices(), 4);
+    EXPECT_EQ(g_8.n_edges(), 5);
 }
 
 TEST(KGraph, Iterator_Constructor)
@@ -74,6 +129,7 @@ TEST(KGraph, Iterator_Constructor)
 
     static_assert(std::is_same_v<decltype(g_1)::vertex_type, int>);
     static_assert(std::is_same_v<decltype(g_1)::edge_type, char>);
+    static_assert(decltype(g_1)::weighted());
     EXPECT_EQ(g_1.n_vertices(), 4);
     EXPECT_EQ(g_1.n_edges(), 5);
 
@@ -88,10 +144,11 @@ TEST(KGraph, Iterator_Constructor)
 
     static_assert(std::is_same_v<decltype(g_2)::vertex_type, int>);
     static_assert(std::is_same_v<decltype(g_2)::edge_type, int>);
+    static_assert(decltype(g_2)::weighted());
     EXPECT_EQ(g_2.n_vertices(), 4);
     EXPECT_EQ(g_2.n_edges(), 5);
 
-    // construction from a tuple-like standard class
+    // construction from a standard tuple-like class
     std::vector vec_3{std::array<int, 3>{1, 2, 0},
                       std::array<int, 3>{1, 3, 0},
                       std::array<int, 3>{2, 3, 0},
@@ -102,10 +159,11 @@ TEST(KGraph, Iterator_Constructor)
 
     static_assert(std::is_same_v<decltype(g_3)::vertex_type, int>);
     static_assert(std::is_same_v<decltype(g_3)::edge_type, int>);
+    static_assert(decltype(g_3)::weighted());
     EXPECT_EQ(g_3.n_vertices(), 4);
     EXPECT_EQ(g_3.n_edges(), 5);
 
-    // construction from a tuple-like user-defined class
+    // construction from a user-defined tuple-like class
     std::vector vec_4{TupleLike{1, 2, 'p'},
                       TupleLike{1, 3, 'q'},
                       TupleLike{2, 3, 'r'},
@@ -116,8 +174,69 @@ TEST(KGraph, Iterator_Constructor)
 
     static_assert(std::is_same_v<decltype(g_4)::vertex_type, int>);
     static_assert(std::is_same_v<decltype(g_4)::edge_type, char>);
+    static_assert(decltype(g_4)::weighted());
     EXPECT_EQ(g_4.n_vertices(), 4);
     EXPECT_EQ(g_4.n_edges(), 5);
+
+    // construction of unweighted graph from std::tuple
+    std::vector vec_5{std::tuple{1, 2},
+                      std::tuple{1, 3},
+                      std::tuple{2, 3},
+                      std::tuple{2, 4},
+                      std::tuple{3, 4}};
+
+    graphs::KGraph g_5(vec_5.begin(), vec_5.end());
+
+    static_assert(std::is_same_v<decltype(g_5)::vertex_type, int>);
+    static_assert(std::is_same_v<decltype(g_5)::edge_type, void>);
+    static_assert(!decltype(g_5)::weighted());
+    EXPECT_EQ(g_5.n_vertices(), 4);
+    EXPECT_EQ(g_5.n_edges(), 5);
+
+    // construction of unweighted graph from std::pair
+    std::vector vec_6{std::pair{1, 2},
+                      std::pair{1, 3},
+                      std::pair{2, 3},
+                      std::pair{2, 4},
+                      std::pair{3, 4}};
+
+    graphs::KGraph g_6(vec_6.begin(), vec_6.end());
+
+    static_assert(std::is_same_v<decltype(g_6)::vertex_type, int>);
+    static_assert(std::is_same_v<decltype(g_6)::edge_type, void>);
+    static_assert(!decltype(g_6)::weighted());
+    EXPECT_EQ(g_6.n_vertices(), 4);
+    EXPECT_EQ(g_6.n_edges(), 5);
+
+    // construction of unweighted graph from standard pair-like class
+    std::vector vec_7{std::array<int, 2>{1, 2},
+                      std::array<int, 2>{1, 3},
+                      std::array<int, 2>{2, 3},
+                      std::array<int, 2>{2, 4},
+                      std::array<int, 2>{3, 4}};
+
+    graphs::KGraph g_7(vec_7.begin(), vec_7.end());
+
+    static_assert(std::is_same_v<decltype(g_7)::vertex_type, int>);
+    static_assert(std::is_same_v<decltype(g_7)::edge_type, void>);
+    static_assert(!decltype(g_7)::weighted());
+    EXPECT_EQ(g_7.n_vertices(), 4);
+    EXPECT_EQ(g_7.n_edges(), 5);
+
+    // construction of unweighted graph from user-defined pair-like class
+    std::vector vec_8{PairLike{1, 2},
+                      PairLike{1, 3},
+                      PairLike{2, 3},
+                      PairLike{2, 4},
+                      PairLike{3, 4}};
+
+    graphs::KGraph g_8(vec_8.begin(), vec_8.end());
+
+    static_assert(std::is_same_v<decltype(g_8)::vertex_type, int>);
+    static_assert(std::is_same_v<decltype(g_8)::edge_type, void>);
+    static_assert(!decltype(g_8)::weighted());
+    EXPECT_EQ(g_8.n_vertices(), 4);
+    EXPECT_EQ(g_8.n_edges(), 5);
 }
 
 TEST(KGraph, Weight)
